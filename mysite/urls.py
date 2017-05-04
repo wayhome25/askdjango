d3787f4fefc981e9f3761d8c095821caf57e1a76
wayhome25 <siwabada@gmail.com>
2017-05-04 16:23:10 +0900

    accounts 앱 생성/등록

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,7 @@ from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.post_list),
+    url(r'^accounts/', include('accounts.urls', namespace='urls')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
 ]
 
