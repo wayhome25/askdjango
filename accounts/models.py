82b607df8e66049cc69fd8bd1b8e64a76d243270
wayhome25 <siwabada@gmail.com>
2017-05-04 17:16:08 +0900

    accounts.models.Profile 모델 필드옵션 수정

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,8 @@
 # accounts/models.py
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User) # FIXME: BAD CASE!!
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
