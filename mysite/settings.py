90269b00ce5cc70d36d40e6fef2f623a3272efaa
wayhome25 <siwabada@gmail.com>
2017-05-05 12:18:46 +0900

    프로젝트 레벨에서 사용할 template dir 설정, 템플릿 상속 적용

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'askdjango', 'templates') # 프로젝트 레벨에서 사용할 템플릿 경로
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
