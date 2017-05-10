b6b848d8e5e0a93add293b7da453017197cd2592
wayhome25 <siwabada@gmail.com>
2017-05-10 16:30:19 +0900

    미디어 파일 서빙을 위한 설정 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@
 
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 # from django.views.generic import RedirectView
@@ -17,6 +18,10 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls', namespace='blog')),
 	url(r'^dojo/', include('dojo.urls', namespace='dojo')),
 ]
+
+# media 파일 서빙을 위한 설정 - static files와는 다르게 개발서버에서 서빙 미지원, 개발 편의성 목적으로 직접 서빙 Rule 추가
+# 단, settings.DEBUG = False일때는 static 함수에서 빈 리스트를 리턴
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # django-debug-toolbar 세팅
 # if settings.DEBUG:
