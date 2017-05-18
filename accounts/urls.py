4a1b07639fed64f01b2f89387084d3f20a0b23b4
wayhome25 <siwabada@gmail.com>
2017-05-18 22:20:15 +0900

    로그인/로그아웃 링크에 next 인자 지정

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
 	url(r'^login/$', auth_views.login, name='login', kwargs={'template_name': 'accounts/login_form.html'}),
 	url(r'^logout/$', auth_views.logout, name='logout', kwargs={'next_page': settings.LOGIN_URL}),
 	url(r'^profile/$', views.profile, name='profile'),
-]
+]  
