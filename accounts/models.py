ad58cd57d47f8c31dc686f935a46232d7be2e9fa
wayhome25 <siwabada@gmail.com>
2017-05-19 20:08:19 +0900

    회원가입시에 휴대폰, 주소 필드 추가

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,9 @@ from django.conf import settings
 from django.db import models
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
-    phone_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=50)
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
+	phone_number = models.CharField(max_length=20)
+	address = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.user.username
